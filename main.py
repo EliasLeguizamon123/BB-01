9a1414effd030926286538dba8ec87a8934bb3f1
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2022-12-27 15:24:31 -0300

    new functionality combinated Balance, fees percent maker and comission

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from bitsoEndpoint import bitsoGetPayload, getTickerUsdToArs
+from bitsoEndpoint import bitsoGetPayload, getTickerUsdToArs, getMyBalance, getFeesForOperate
 from time import sleep
 
 def main() :
     bitsoGetPayload()
     while True:
         getTickerUsdToArs()
+        getFeesForOperate()
         sleep(1)
     
 if __name__ == '__main__':
